95e2ba88565071c32332f1cbded79cf3862ef6e5
VikasSiwal <vikas.siwal.met22@itbhu.ac.in>
2025-06-06 19:53:33 +0530

    Added 03_conversion file

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,55 @@
+let score = "33"
+
+console.log(typeof(score));
+
+let valueInNUmber = Number(score) //string to number conversion
+console.log(typeof(valueInNUmber));
+console.log(valueInNUmber);
+
+//"33"->33
+//"33abc"->NaN(not a number)
+//"true"->1; 
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1->true; 0->false
+//""->false
+//"vicky"->true
+
+let num= 35
+let stringNumber=String(num);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+//**********Operations*********** */
+let value=3;
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2**3);
+// console.log(2*3);
+// console.log(2%3);
+
+let str1="hello"
+let str2= " vicky"
+
+let str3= str1+str2
+// console.log(str3);
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 //ese readability achhi nhi lgti
+
+// jyada gyan ke loiye niche wali link pr jaye
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
