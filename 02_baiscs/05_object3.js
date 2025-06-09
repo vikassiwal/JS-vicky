bccd19ab4b90ae2bb0023fe04e06ae2f4a5143f6
VikasSiwal <vikas.siwal.met22@itbhu.ac.in>
2025-06-09 19:44:40 +0530

    json talk

---
+++ 02_baiscs/05_object3.js
@@ -0,0 +1,24 @@
+//objects
+//destructuring
+const course ={
+    name:"js in hindi",
+    price:"999",
+    instructor:"hitesh"
+}
+// course.instructor
+
+const {instructor:ins}=course
+console.log(ins);
+
+//json
+// {
+//     "name":"vicky",
+//     "courseName": "js in eng",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
